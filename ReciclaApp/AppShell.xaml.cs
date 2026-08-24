b8cee2ce05bf0ec3701d0a97d1122d8db537ba76
VitoCold <39446918+VitoCold@users.noreply.github.com>
2026-08-23 22:26:24 -0500

    Agregar archivos de proyecto.

---
+++ ReciclaApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ReciclaApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ReciclaApp/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ReciclaApp")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ReciclaApp.Pages")]
