9edaaf576f878b617a426ac74323bbeea00c34e3
VitoCold <39446918+VitoCold@users.noreply.github.com>
2026-09-22 09:38:27 -0500

    Add residues navigation handlers

--- ReciclaApp/Pages/DetalleRegistroResiduosPage.xaml.cs
+++ ReciclaApp/Pages/DetalleRegistroResiduosPage.xaml.cs
@@ -1,3 +1,5 @@
+using ReciclaApp.Navigation;
+
 namespace ReciclaApp.Pages;
 
 public partial class DetalleRegistroResiduosPage : ContentPage
@@ -5,5 +7,30 @@ public partial class DetalleRegistroResiduosPage : ContentPage
     public DetalleRegistroResiduosPage()
     {
         InitializeComponent();
+    }
+
+    private async void OnBackTapped(object sender, TappedEventArgs e)
+    {
+        await AppNavigator.VolverAsync();
+    }
+
+    private async void OnDetalleTapped(object sender, TappedEventArgs e)
+    {
+        await AppNavigator.IrADetalleRegistroAsync();
+    }
+
+    private async void OnDisposicionTapped(object sender, TappedEventArgs e)
+    {
+        await AppNavigator.IrADisposicionDelRegistroAsync();
+    }
+
+    private async void OnAgregarResiduoClicked(object sender, EventArgs e)
+    {
+        await AppNavigator.IrARegistrarResiduoAsync();
+    }
+
+    private async void OnResiduoTapped(object sender, TappedEventArgs e)
+    {
+        await AppNavigator.IrADetalleResiduoAsync();
     }
 }
