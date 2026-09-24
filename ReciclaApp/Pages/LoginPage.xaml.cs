16223caa1f24f253af2d46eb3376af4ee185651d
VitoCold <39446918+VitoCold@users.noreply.github.com>
2026-09-23 20:04:28 -0500

    Make SecureStorage dependency explicit

--- ReciclaApp/Pages/LoginPage.xaml.cs
+++ ReciclaApp/Pages/LoginPage.xaml.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Storage;
 using Recicla.Shared.Contracts;
 using Recicla.Shared.Services;
 using ReciclaApp.Navigation;
