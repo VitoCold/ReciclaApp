69cc083f8c8797614b26b4c89189ea47ab701c47
VitoCold <39446918+VitoCold@users.noreply.github.com>
2026-09-25 13:09:40 -0500

    Registrar consulta de registros por control

--- ReciclaApi/Program.cs
+++ ReciclaApi/Program.cs
@@ -82,6 +82,7 @@ builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<ICatalogoService, CatalogoService>();
 builder.Services.AddScoped<IRegistroService, RegistroService>();
 builder.Services.AddScoped<IControlGeneracionService, ControlGeneracionService>();
+builder.Services.AddScoped<IControlGeneracionRegistroConsultaService, ControlGeneracionRegistroConsultaService>();
 builder.Services.AddScoped<DatabaseInitializer>();
 
 var app = builder.Build();
